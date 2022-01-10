050428fa50e2dc00f2ddd7401a9645a890b556c9
bHicks2112 <79944750+bHicks2112@users.noreply.github.com>
2022-01-10 12:15:37 -0700

    Add project files.

---
+++ MyFirstASPNetWebApp/wwwroot/js/fanmail.js
@@ -0,0 +1,13 @@
+﻿////document.getElementById('btnSend').addEventListener("click", function () {
+////    alert("From: " + document.getElementById('txtFrom').value +
+////        ", Subject: " + document.getElementById("txtSubject").value +
+////        ", Mesage: " + document.getElementById("txt.Message").value);
+////})
+
+$("#btnSend").click(function () {
+    alert('From: ' + $('#txtFrom').val() +
+        ", Subject: " + $('#txtSubject').val() +
+        ", Message: " + $('#txtMessage').val());
+
+    $('#picGanderson').fadeToggle();
+})
